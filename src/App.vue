70296d4582c02d509c9a601925d8d6729fd2547e
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-13 17:14:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+    components: {
+        AppTitle
+    },
+    data() {
+        return {}
+    }
+}
+
+</script>
+
+<template>
+    <AppTitle/>
+</template>
+
+<style scoped></style>
